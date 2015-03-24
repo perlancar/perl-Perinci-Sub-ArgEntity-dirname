35bebc8b6969b4714c2b851da22789bf3aca5347
perlancar (@pc-office) <perlancar@gmail.com>
2015-03-24 18:54:57 +0700

    Initial commit

---
+++ lib/Perinci/Sub/ArgEntity/dirname.pm
@@ -0,0 +1,23 @@
+package Perinci::Sub::ArgEntity::dirname;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+use Complete::Util ();
+
+sub complete_arg_val {
+    Complete::Util::complete_file(@_, filter=>'d');
+}
+
+1;
+#ABSTRACT: Data and code related to function arguments of entity type 'dirname'
+
+=for Pod::Coverage ^(.+)$
+
+=head1 SEE ALSO
+
+L<Perinci::Sub::ArgEntity>
